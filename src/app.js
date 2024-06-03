43cfb924389e68d740b44c5eeb33ef4886e79832
Leonardo <rodonileo5@gmail.com>
2024-06-03 09:02:05 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import { config } from "dotenv";
+config();
+
+import bookRoutes from "./routes/book.routes.js";
+
+const app = express();
+
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGO_URL, { dbName: process.env.MONGO_DB_NAME });
+const db = mongoose.connection;
+
+app.use("/books", bookRoutes);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
